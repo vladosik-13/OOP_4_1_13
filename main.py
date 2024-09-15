04daa9f1485aaece6dfd656ae1655e53848dc5ac
Владислав Кошелев <testvlad59@gmail.com>
2024-09-15 12:41:50 +0300

    перенес описание классов из main в src.classes_description

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-
-class Category:
-    name: str
-    description: str
-    products: list
-    category_count = 0
-    product_count = 0
-
-    def __init__(self, name, description, products):
-        self.name = name
-        self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count += len(products)
+from src.classes_description import Product, Category
 
 
 if __name__ == "__main__":
