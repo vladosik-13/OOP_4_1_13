469135ff24a3c93c46fc4a8f9814072623bb44e1
Владислав Кошелев <testvlad59@gmail.com>
2024-09-17 20:26:58 +0300

    Для класса Category список товаров приватным атрибутом сделал приватным. Для добавления товаров в категорию сделан специальный метод add_product() в классе Category

--- src/classes_description.py
+++ src/classes_description.py
@@ -14,13 +14,16 @@ class Product:
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self):
+        return {self.__products}
