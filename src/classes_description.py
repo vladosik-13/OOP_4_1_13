8353a358ec6fb1841cb23e9f90a4ff76f9c86d43
Владислав Кошелев <testvlad59@gmail.com>
2024-09-29 00:55:13 +0300

    добавлены два дочерних класса от класса Product - Smartphone и LawnGrass

--- src/classes_description.py
+++ src/classes_description.py
@@ -64,3 +64,30 @@ class Category:
     def __str__(self) -> str:
         total_quantity = sum(product.quantity for product in self.__products)
         return f"{self.name}, общее количество продуктов: {total_quantity} шт."
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+    def __str__(self) -> str:
+        return (super().__str__() +
+                f", Эффективность: {self.efficiency}, Модель: {self.model}, "
+                f"Встроенная память: {self.memory} ГБ, Цвет: {self.color}")
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __str__(self) -> str:
+        return (super().__str__() +
+                f", Страна производства: {self.country}, Срок прорастания: {self.germination_period} дней, "
+                f"Цвет: {self.color}")
